733db7774454d73d3123eae508697901987716f6
Shitanshu Tripathi <tripathis1106@gmail.com>
2023-10-15 20:58:12 +0530

    Validation Added

--- Screens/ForgotPass.jsx
+++ Screens/ForgotPass.jsx
@@ -1,62 +1,94 @@
-import { View, Text, Dimensions, Modal, Image } from "react-native";
+import { View, Text, Dimensions, Modal, Image, TextInput } from "react-native";
 import React, { useState } from "react";
 import { Styles } from "../Components/Styles";
-import Input from "../Components/InputComponent/Input";
 import { COLORS, FONTS, SIZES } from "../constants/theme";
 import Button from "../Components/Button";
 import { modal } from "./Styles/screenStyles";
 import icons from "../constants/icons";
+import { styles } from "../Components/InputComponent/styles";
+import { Formik } from "formik";
+import { loginSchema } from "../Utils/Schema";
 
 const { height, width } = Dimensions.get("window");
 
 const ForgotPass = (props) => {
+  const [visible, setVisible] = useState(false);
 
-    const [visible, setVisible] = useState(false);
+  const handleSubmit = (values) => {
+    setVisible(true);
+    setTimeout(() => {
+      props.navigation.replace("ResetPass");
+    }, 2000);
+  };
 
-    const handlePress = () => {
-        setVisible(true);
-        setTimeout(() => {
-            props.navigation.replace('ResetPass');
-        }, 2000);
-    };
+  return (
+    <View style={Styles.container}>
+      <View style={[Styles.container2, { paddingTop: height * 0.1 }]}>
+        <Text style={[Styles.headText, { textAlign: "center" }]}>
+          Forgot Password?
+        </Text>
+        <Text
+          style={{
+            fontFamily: FONTS.Arial,
+            textAlign: "center",
+            fontSize: height * 0.016,
+            marginVertical: height * 0.01,
+          }}>
+          Enter the email address associated with your account and we’ll send
+          you a LINK to reset your password
+        </Text>
 
-    return (
-        <View style={Styles.container}>
-            <View style={[Styles.container2, { paddingTop: height * 0.1 }]}>
-                <Text
-                    style={[Styles.headText, { textAlign: "center" }]}>
-                    Forgot Password?
-                </Text>
-                <Text
-                    style={{
-                        fontFamily: FONTS.Arial,
-                        textAlign: "center",
-                        fontSize: height * 0.016,
-                        marginVertical: height * 0.01,
-                    }}>
-                    Enter the email address associated with your account and we’ll send
-                    you a LINK to reset your password
-                </Text>
-                <Input placeholder={"Enter your email"} top={height * 0.02} />
-                <Button
-                    title={"SEND"}
-                    bgColor={COLORS.red}
-                    onPress={() => handlePress()}
-                />
-                {
-                    <Modal transparent visible={visible} >
-                        <View style={modal.container}>
-                            <View style={modal.centerView}>
-                                <Image source={icons.success} style={modal.image} />
-                                <Text style={[Styles.headText, { marginTop: height * 0.07 }]}>Thank You!</Text>
-                                <Text style={{textAlign:'center'}}>LINK has been successfully sent to your registered email address</Text>
-                            </View>
-                        </View>
-                    </Modal>
-                }
+        {/* <<---------- Formik Validation --------->> */}
+        <Formik
+          validationSchema={loginSchema}
+          initialValues={{ email: "" }}
+          onSubmit={handleSubmit}>
+          {({
+            handleChange,
+            setFieldTouched,
+            handleSubmit,
+            values,
+            touched,
+            errors,
+          }) => (
+            <View style={{marginTop: height*0.05}}>
+              <TextInput
+                style={[styles.input, { color: "#010",marginBottom:height*0.01 }]}
+                placeholder={"Enter your email"}
+                onChangeText={handleChange("email")}
+                onBlur={() => setFieldTouched("email")}
+                value={values.password}
+                maxLength={30}
+              />
+              <Text style={[Styles.errors,{marginLeft: width*0.04}]}>{touched.email && errors.email}</Text>
+
+              <Button
+                title={"SEND"}
+                bgColor={COLORS.red}
+                onPress={() => handleSubmit()}
+              />
             </View>
-        </View>
-    );
+          )}
+        </Formik>
+        {
+          <Modal transparent visible={visible}>
+            <View style={modal.container}>
+              <View style={modal.centerView}>
+                <Image source={icons.success} style={modal.image} />
+                <Text style={[Styles.headText, { marginTop: height * 0.07 }]}>
+                  Thank You!
+                </Text>
+                <Text style={{ textAlign: "center" }}>
+                  LINK has been successfully sent to your registered email
+                  address
+                </Text>
+              </View>
+            </View>
+          </Modal>
+        }
+      </View>
+    </View>
+  );
 };
 
 export default ForgotPass;
