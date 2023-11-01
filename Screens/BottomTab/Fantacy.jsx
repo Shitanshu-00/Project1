eaa8e9be897eab257c2569de789c588b341fee22
Shitanshu Tripathi <tripathis1106@gmail.com>
2023-11-01 20:21:22 +0530

    Fantacy Screen Completed

--- Screens/BottomTab/Fantacy.jsx
+++ Screens/BottomTab/Fantacy.jsx
@@ -1,23 +1,30 @@
-import { View, Text, SafeAreaView } from "react-native";
+import { View, Text, SafeAreaView, Dimensions } from "react-native";
 import React from "react";
 import { COLORS, SIZES } from "../../constants/theme";
 import { bottomStyles } from "./Styles.BottomTab";
 
 const Fantacy = () => {
   return (
-    <SafeAreaView style={[bottomStyles.container,{alignItems:'center', justifyContent:'center'}]}>
-      <View style={bottomStyles.fantacyLogo}>
-        <Text
-          style={{
-            fontSize: SIZES.Large,
-            color: COLORS.white,
-            fontWeight: "bold",
-          }}>
-          COMING SOON!
-        </Text>
+    <SafeAreaView style={bottomStyles.container}>
+      <View style={{ backgroundColor: COLORS.black, height: height * 0.07, alignItems: 'flex-start', justifyContent: 'center', paddingHorizontal: width * 0.04 }}>
+        <Text style={{ color: COLORS.white, fontSize: height * 0.03, fontWeight: '800' }}>Fantacy</Text>
+      </View >
+      <View style={{flex:1, alignItems:'center', justifyContent:'center'}}>
+        <View style={bottomStyles.fantacyLogo}>
+          <Text
+            style={{
+              fontSize: SIZES.Large,
+              color: COLORS.white,
+              fontWeight: "bold",
+            }}>
+            COMING SOON!
+          </Text>
+        </View>
       </View>
     </SafeAreaView>
   );
 };
 
 export default Fantacy;
+
+const { height, width } = Dimensions.get('window');
