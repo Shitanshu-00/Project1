290e51d8a4b16127def09c1eebbab26390f90547
Shitanshu Tripathi <tripathis1106@gmail.com>
2023-11-02 11:37:31 +0530

    ScrollView error fixed

--- Screens/BottomTab/Matches/Recent.jsx
+++ Screens/BottomTab/Matches/Recent.jsx
@@ -7,10 +7,77 @@ import Database from '../../../Utils/db.json';
 import { COLORS } from '../../../constants/theme';
 
 const Recent = () => {
+
+  const BottomFlatlist = () => {
+    return (
+      <View>
+        <View style={{ marginBottom: height * 0.02 }}>
+          <Text style={[Styles.title, { color: "#010", fontWeight: "800", paddingLeft: width * 0.04 }]}>
+            LEAGUE
+          </Text>
+          <View style={{ backgroundColor: COLORS.red, marginVertical: height * 0.01, height: height * 0.05, paddingLeft: width * 0.04, justifyContent: 'center' }}>
+            <Text style={{ color: "#fff", fontSize: height * 0.02 }}>T20 WC East Asia Pacific</Text>
+          </View>
+
+          {/* <<-------------------------- League Matches --------------------------->> */}
+            <FlatList
+              showsVerticalScrollIndicator={false}
+              data={Database.matches[0]['T20 WC East Asia Pacific']}
+              renderItem={({ item }) => {
+                return (
+                  <TouchableOpacity style={[bottomStyles.ScrollSm, { width: width * 0.9, marginBottom: height * 0.025, paddingLeft: width * 0.04, height: height * 0.15, marginHorizontal: width*0.04 }]}>
+                    <View style={[bottomStyles.rowView, { marginBottom: height * 0.01, justifyContent: 'space-between', paddingRight: width * 0.04 }]}>
+                      <Text style={{ color: "#fff", fontSize: height * 0.014 }}>
+                        {item.match} Match
+                      </Text>
+                      <Text style={{ color: "#fff", fontSize: height * 0.014 }}>
+                        {item.group}
+                      </Text>
+                      <Text style={{ color: "#fff", fontSize: height * 0.014 }}>
+                        {item.venue}
+                      </Text>
+                    </View>
+
+                    <View style={bottomStyles.rowView}>
+                      <Image source={icons.flag_IN} style={{ marginRight: width * 0.02 }} />
+                      <Text style={{ color: "#fff", fontSize: height * 0.016 }}>
+                        {item.country1.name}
+                      </Text>
+                      <Text style={{ color: "#fff", fontSize: height * 0.016, position: "absolute", right: width * 0.04 }}>
+                        *{item.country1.runs}/{item.country1.wickets}(
+                        {item.country1.overs}ov)
+                      </Text>
+                    </View>
+                    <View style={[bottomStyles.rowView, { marginVertical: height * 0.01 }]}>
+                      <Image
+                        source={icons.flag_SA}
+                        style={{ marginRight: width * 0.02 }}
+                      />
+                      <Text style={{ color: "#fff", fontSize: height * 0.016 }}>
+                        {item.country2.name}
+                      </Text>
+                      <Text style={{ color: "#fff", fontSize: height * 0.016, position: "absolute", right: width * 0.04 }}>
+                        *{item.country2.runs}/{item.country2.wickets}(
+                        {item.country2.overs}ov)
+                      </Text>
+                    </View>
+
+
+                    <Text style={{ color: item.status === "LIVE" ? COLORS.red : "#00920F", fontSize: height * 0.016, textAlign: 'center' }}>
+                      {item.highlight}
+                    </Text>
+                  </TouchableOpacity>
+                );
+              }}
+            />
+        </View>
+      </View>
+    )
+  }
+
   return (
-    <ScrollView>
-      <View style={{ marginTop: height * 0.01 }}>
-        <Text style={[Styles.title, { color: "#010", fontWeight: "800", paddingLeft: width * 0.04 }]}>
+    <View style={bottomStyles.container}>
+        <Text style={[Styles.title, { color: "#010", fontWeight: "800", paddingLeft: width * 0.04, marginTop: height*0.01 }]}>
           INTERNATIONAL
         </Text>
         <View style={{ backgroundColor: COLORS.red, marginVertical: height * 0.01, height: height * 0.05, paddingLeft: width * 0.04, justifyContent: 'center' }}>
@@ -18,13 +85,12 @@ const Recent = () => {
         </View>
 
         {/* <<-------------------------- International Matches --------------------------->> */}
-        <View style={{ flex: 1, paddingHorizontal: width * 0.05 }}>
           <FlatList
             showsVerticalScrollIndicator={false}
             data={Database.matches[0]['ICC World Cup 2022']}
-            renderItem={({ item, index }) => {
+            renderItem={({ item }) => {
               return (
-                <TouchableOpacity style={[bottomStyles.ScrollSm, { width: width * 0.9, marginBottom: height * 0.025, paddingLeft: width * 0.04, height: height * 0.15 }]}>
+                <TouchableOpacity style={[bottomStyles.ScrollSm, { width: width * 0.9, marginBottom: height * 0.025, paddingLeft: width * 0.04, height: height * 0.15, marginHorizontal: width*0.04 }]}>
                   <View style={[bottomStyles.rowView, { marginBottom: height * 0.01, justifyContent: 'space-between', paddingRight: width * 0.04 }]}>
                     <Text style={{ color: "#fff", fontSize: height * 0.014 }}>
                       {item.match} Match
@@ -68,73 +134,10 @@ const Recent = () => {
                 </TouchableOpacity>
               );
             }}
+            ListFooterComponent={<BottomFlatlist/>}
           />
         </View>
-      </View>
 
-      <View style={{ marginBottom: height * 0.02 }}>
-        <Text style={[Styles.title, { color: "#010", fontWeight: "800", paddingLeft: width * 0.04 }]}>
-          LEAGUE
-        </Text>
-        <View style={{ backgroundColor: COLORS.red, marginVertical: height * 0.01, height: height * 0.05, paddingLeft: width * 0.04, justifyContent: 'center' }}>
-          <Text style={{ color: "#fff", fontSize: height * 0.02 }}>T20 WC East Asia Pacific</Text>
-        </View>
-
-        {/* <<-------------------------- League Matches --------------------------->> */}
-        <View style={{ flex: 1, paddingHorizontal: width * 0.05 }}>
-          <FlatList
-            showsVerticalScrollIndicator={false}
-            data={Database.matches[0]['T20 WC East Asia Pacific']}
-            renderItem={({ item, index }) => {
-              return (
-                <TouchableOpacity style={[bottomStyles.ScrollSm, { width: width * 0.9, marginBottom: height * 0.025, paddingLeft: width * 0.04, height: height * 0.15 }]}>
-                  <View style={[bottomStyles.rowView, { marginBottom: height * 0.01, justifyContent: 'space-between', paddingRight: width * 0.04 }]}>
-                    <Text style={{ color: "#fff", fontSize: height * 0.014 }}>
-                      {item.match} Match
-                    </Text>
-                    <Text style={{ color: "#fff", fontSize: height * 0.014 }}>
-                      {item.group}
-                    </Text>
-                    <Text style={{ color: "#fff", fontSize: height * 0.014 }}>
-                      {item.venue}
-                    </Text>
-                  </View>
-
-                  <View style={bottomStyles.rowView}>
-                    <Image source={icons.flag_IN} style={{ marginRight: width * 0.02 }} />
-                    <Text style={{ color: "#fff", fontSize: height * 0.016 }}>
-                      {item.country1.name}
-                    </Text>
-                    <Text style={{ color: "#fff", fontSize: height * 0.016, position: "absolute", right: width * 0.04 }}>
-                      *{item.country1.runs}/{item.country1.wickets}(
-                      {item.country1.overs}ov)
-                    </Text>
-                  </View>
-                  <View style={[bottomStyles.rowView, { marginVertical: height * 0.01 }]}>
-                    <Image
-                      source={icons.flag_SA}
-                      style={{ marginRight: width * 0.02 }}
-                    />
-                    <Text style={{ color: "#fff", fontSize: height * 0.016 }}>
-                      {item.country2.name}
-                    </Text>
-                    <Text style={{ color: "#fff", fontSize: height * 0.016, position: "absolute", right: width * 0.04 }}>
-                      *{item.country2.runs}/{item.country2.wickets}(
-                      {item.country2.overs}ov)
-                    </Text>
-                  </View>
-
-
-                  <Text style={{ color: item.status === "LIVE" ? COLORS.red : "#00920F", fontSize: height * 0.016, textAlign: 'center' }}>
-                    {item.highlight}
-                  </Text>
-                </TouchableOpacity>
-              );
-            }}
-          />
-        </View>
-      </View>
-    </ScrollView>
   )
 }
 
