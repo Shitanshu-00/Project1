8981fe78d0ac20c9b0d1039d248212b574e8c9fa
Shitanshu Tripathi <tripathis1106@gmail.com>
2023-11-14 21:54:05 +0530

    About Screen

--- Screens/About.jsx
+++ Screens/About.jsx
@@ -8,15 +8,8 @@ const { height, width } = Dimensions.get("window");
 const About = (props) => {
   return (
     <View
-      style={[
-        Styles.container,
-        { backgroundColor: "#fff", paddingHorizontal: width * 0.06},
-      ]}>
-       <View
-        style={[
-          Styles.rowView,
-          { paddingVertical: height * 0.01, alignItems: "center" },
-        ]}>
+      style={[Styles.container, { backgroundColor: "#fff", paddingHorizontal: width * 0.06 }]}>
+      <View style={[Styles.rowView, { paddingVertical: height * 0.01, alignItems: "center" }]}>
         <TouchableOpacity onPress={() => props.navigation.goBack()}>
           <Ionicons
             name="chevron-back-circle"
@@ -24,17 +17,13 @@ const About = (props) => {
             color="black"
           />
         </TouchableOpacity>
-        <Text
-          style={[
-            Styles.headText,
-            { fontWeight: "bold", marginLeft: width * 0.2 },
-          ]}>
-         About Us
+        <Text style={[Styles.headText, { fontWeight: "bold", marginLeft: width * 0.2 }]}>
+          About Us
         </Text>
       </View>
 
-      <View style={{ marginTop: height*0.01}}>
-        <Text style={{textAlign:'justify'}}>India strolled to a nine-wicket win against Thailand in the Women's T20 Asia Cup after spinners Deepti Sharma, Sneh Rana and Rajeshwari Gayakwad shared seven wickets between them. Thailand were restricted to 37, their third-lowest total in the Asia Cup, which table-toppers India then chased down in six overs, losing just one wicket.</Text>
+      <View style={{ marginTop: height * 0.01 }}>
+        <Text style={{ textAlign: 'justify', fontFamily:'Lato-Medium', fontSize: height*0.02 }}>Hello Everyone, I am Shitanshu Tripathi. I am a React Native Developer and this is a Demo App of Fantacy Cricket category which display informations such as Live Cricket and other data. In this project I used Firebase for Login and Authentication and other third party libraries.</Text>
       </View>
     </View>
   )
